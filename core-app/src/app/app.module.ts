666539249ecd4d6bad35ac3478031ad6f5cc1056
Tansu Dasli <tansudasli@gmail.com>
2019-07-30 23:43:50 +0300

    Initialize - Pages

--- core-app/src/app/app.module.ts
+++ core-app/src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material/material.module';
+import { LoginComponent } from './authz/login/login.component';
+import { SigninComponent } from './authz/signin/signin.component';
+import { RegisterComponent } from './authz/register/register.component';
+import { VerificationComponent } from './authz/verification/verification.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SigninComponent,
+    RegisterComponent,
+    VerificationComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
