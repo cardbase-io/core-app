729b7dde47688ad8915648051afcd6ee78bb7a88
Tansu Dasli <tansudasli@gmail.com>
2019-07-31 00:22:30 +0300

    Initialize - Pages

---
+++ core-app/src/app/components/master.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Master} from '../models/Master';
+
+@Component({
+  selector: 'app-master',
+  templateUrl: './master.component.html',
+  styleUrls: ['./master.component.css']
+})
+export class MasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
