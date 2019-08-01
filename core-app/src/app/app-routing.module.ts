009e5dc43b93113586ea5b1e507928acc0aa7981
Tansu Dasli <tansudasli@gmail.com>
2019-08-02 00:10:12 +0300

    initialize basic animations #8

--- core-app/src/app/app-routing.module.ts
+++ core-app/src/app/app-routing.module.ts
@@ -7,20 +7,19 @@ import { RegisterComponent } from './components/authz/register/register.componen
 import { LoginComponent } from './components/authz/login/login.component';
 import { VerificationComponent } from './components/authz/verification/verification.component';
 import { MasterComponent } from './components/master.component';
-import {EligibilityComponent} from './components/authz/eligibility/eligibility.component';
-import {VerifyComponent} from './components/authz/verify/verify.component';
-
+import { EligibilityComponent } from './components/authz/eligibility/eligibility.component';
+import { VerifyComponent } from './components/authz/verify/verify.component';
 
 const routes: Routes = [
-  { path: 'index', component: MasterComponent,  data: { title: 'Master' }  },
-  { path: 'detail/:id', component: DetailComponent,  data: { title: 'Detail' }  },
-  { path: 'detail', component: DetailComponent,  data: { title: 'Detail' }  },
-  { path: 'authz/signin', component: SigninComponent,  data: { title: 'Sign in' }  },
-  { path: 'authz/login', component: LoginComponent,  data: { title: 'Login' }  },
-  { path: 'authz/verify', component: VerifyComponent,  data: { title: 'Verify' }  },
-  { path: 'authz/verification', component: VerificationComponent,  data: { title: 'Verification' }  },
-  { path: 'authz/register', component: RegisterComponent,  data: { title: 'Register' }  },
-  { path: 'eligibility', component: EligibilityComponent,  data: { title: 'Eligibility' }  },
+  { path: 'index', component: MasterComponent,  data: { title: 'Master', animation: 'masterPage' } },
+  { path: 'detail/:id', component: DetailComponent,  data: { title: 'Detail', animation: 'detailPage' } },
+  { path: 'detail', component: DetailComponent,  data: { title: 'Detail', animation: 'detailPage' } },
+  { path: 'authz/signin', component: SigninComponent,  data: { title: 'Sign in' } },
+  { path: 'authz/login', component: LoginComponent,  data: { title: 'Login' } },
+  { path: 'authz/verify', component: VerifyComponent,  data: { title: 'Verify' } },
+  { path: 'authz/verification', component: VerificationComponent,  data: { title: 'Verification' } },
+  { path: 'authz/register', component: RegisterComponent,  data: { title: 'Register' } },
+  { path: 'eligibility', component: EligibilityComponent,  data: { title: 'Eligibility' } },
   // { path: 'heroes', component: HeroListComponent, data: { title: 'Heroes List' } },
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   // { path: '**', component: PageNotFoundComponent }
--- core-app/src/app/app.component.ts
+++ core-app/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { slideInAnimation } from './animations';
+import { RouterOutlet } from '@angular/router';
+
+// import {
+//   trigger,
+//   state,
+//   style,
+//   animate,
+//   transition,
+//   // animation functions
+// } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [ //animation triggers
+    slideInAnimation
+  ]
 })
 export class AppComponent {
   title = 'carbase.io';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
